f7f67f6bbe0581ac1c3f669c46d46318e39b2983
Brian Yingling <brian@daft.io>
2013-12-14 14:20:54 -0500

    finished lesson 14

---
+++ 14-twitter/percentages/app.js
@@ -0,0 +1,47 @@
+var express = require('express'),
+    app     = express(),
+    twitter = require('ntwitter'),
+    server  = require('http').createServer(app),
+    io      = require('socket.io').listen(server);
+
+var love  = 0,
+    hate  = 0,
+    total = 0;
+
+server.listen(3000);
+
+var twit = new twitter({
+  consumer_key: process.env.LOVE_OR_HATE_TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.LOVE_OR_HATE_TWITTER_CONSUMER_SEC,
+  access_token_key: process.env.LOVE_OR_HATE_TOKEN_KEY,
+  access_token_secret: process.env.LOVE_OR_HATE_TOKEN_SEC
+});
+
+twit.stream('statuses/filter', {track: ['love','hate']}, function(stream) {
+  
+  stream.on('data', function(data) {
+    var text = data.text.toLowerCase();
+    if (text.indexOf('love') !== -1) {
+      love++;
+      total++;
+    }
+    if (text.indexOf('hate') !== -1) {
+      hate++;
+      total++;
+    }
+
+    // emit a tweet event
+    io.sockets.volatile.emit('tweet', {
+      user: data.user.screen_name,
+      text: data.text,
+      love: (love/total) * 100,
+      hate: (hate/total) * 100,
+      total: total
+    });
+  });
+});
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
